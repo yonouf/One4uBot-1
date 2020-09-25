81e473b21d7275e1a2d312d3d562fbad0b3307fa
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2020-09-25 14:32:40 +0700

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -1018,6 +1018,9 @@ async def _(event):
 
 CMD_HELP.update(
     {
-        "admin": "⊙ .pro Promote ⊙ .dem Demote ⊙ .b Ban ⊙ .ub UnBan ⊙ .m Mute ⊙ .um UnMute ⊙ .zo Zombies ⊙ .pin ⊙ .k Kick ⊙ .abl Add Blacklist ⊙ .lbl List Blacklist ⊙ .rbl Remove Blacklist ⊙"
+        "admin": 
+        "⊙ .pro Promote ⊙ .dem Demote ⊙ .b Ban ⊙ .ub UnBan ⊙ .m Mute ⊙ .um UnMute ⊙ .zo Zombies ⊙"
+        " .pin Pin ⊙ .k Kick ⊙ .abl Add Blacklist ⊙ .lbl List Blacklist ⊙ .rbl Remove Blacklist ⊙\n"
+        "⊙ .fb Fed Ban ⊙ .uf Unban Fed ⊙ .af Add Fed ⊙ .df Delete Fed ⊙ .cf Clear All Fed ⊙ .lf List Fed ⊙"
     }
 )
