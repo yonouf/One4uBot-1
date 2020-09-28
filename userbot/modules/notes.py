601e33e7089f02e26d62365abe9d8a0667a0992e
BL4CK_ID <58440372+BL4CKID@users.noreply.github.com>
2020-09-28 12:15:33 +0700

    Update notes.py

--- userbot/modules/notes.py
+++ userbot/modules/notes.py
@@ -11,7 +11,7 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.notes$")
+@register(outgoing=True, pattern="^.xnotes$")
 async def notes_active(svd):
     """ For .notes command, list all of the notes saved in a chat. """
     try:
@@ -30,7 +30,7 @@ async def notes_active(svd):
     await svd.edit(message)
 
 
-@register(outgoing=True, pattern=r"^.clear (\w*)")
+@register(outgoing=True, pattern=r"^.xclear (\w*)")
 async def remove_notes(clr):
     """ For .clear command, clear note with the given name."""
     try:
@@ -45,7 +45,7 @@ async def remove_notes(clr):
         return await clr.edit("`Successfully deleted note:` **{}**".format(notename))
 
 
-@register(outgoing=True, pattern=r"^.save (\w*)")
+@register(outgoing=True, pattern=r"^.xsave (\w*)")
 async def add_note(fltr):
     """ For .save command, saves notes in a chat. """
     try:
@@ -117,7 +117,7 @@ async def incom_note(getnt):
         pass
 
 
-@register(outgoing=True, pattern="^.rmbotnotes (.*)")
+@register(outgoing=True, pattern="^.xrmbotnotes (.*)")
 async def kick_marie_notes(kick):
     """ For .rmbotnotes command, allows you to kick all \
         Marie(or her clones) notes from a chat. """
@@ -143,18 +143,18 @@ async def kick_marie_notes(kick):
         )
 
 
-CMD_HELP.update(
-    {
-        "notes": "\
-#<notename>\
-\nUsage: Gets the specified note.\
-\n\n.save <notename> <notedata> or reply to a message with .save <notename>\
-\nUsage: Saves the replied message as a note with the notename. (Works with pics, docs, and stickers too!)\
-\n\n.notes\
-\nUsage: Gets all saved notes in a chat.\
-\n\n.clear <notename>\
-\nUsage: Deletes the specified note.\
-\n\n.rmbotnotes <marie/rose>\
-\nUsage: Removes all notes of admin bots (Currently supported: Marie, Rose and their clones.) in the chat."
-    }
-)
+#CMD_HELP.update(
+#    {
+#        "notes": "\
+##<notename>\
+#\nUsage: Gets the specified note.\
+#\n\n.save <notename> <notedata> or reply to a message with .save <notename>\
+#\nUsage: Saves the replied message as a note with the notename. (Works with pics, docs, and stickers too!)\
+#\n\n.notes\
+#\nUsage: Gets all saved notes in a chat.\
+#\n\n.clear <notename>\
+#\nUsage: Deletes the specified note.\
+#\n\n.rmbotnotes <marie/rose>\
+#\nUsage: Removes all notes of admin bots (Currently supported: Marie, Rose and their clones.) in the chat."
+#    }
+#)
